f1db4d39f4437b8ec9a2ab61d430ae639a140009
Emory Freitas <ervf@cesar.org.br>
2022-01-24 12:14:31 -0400

    Allow CORS
    Signed-off-by: Emory Freitas <emory.rvf@gmail.com>

---
+++ api/src/main/java/com/sinch/gamehighscore/config/ConfigCORS.java
@@ -0,0 +1,31 @@
+package com.sinch.gamehighscore.config;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class ConfigCORS implements WebMvcConfigurer{
+
+    @Bean
+	CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("*"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET"));
+        configuration.setAllowCredentials(false);
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+	}
+}
--- api/src/main/java/com/sinch/gamehighscore/controller/ScoreController.java
+++ api/src/main/java/com/sinch/gamehighscore/controller/ScoreController.java
@@ -14,10 +14,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@CrossOrigin(origins = {"http//:localhost:8080", "http://localhost:3000"})
 @Controller
 @RequestMapping(path="/score")
 public class ScoreController {
