d57e8809e0d332ecb8d9a58cbf6a834667a3bc94
Emory Freitas <ervf@cesar.org.br>
2022-01-24 13:55:28 -0400

    Fix cors origin
    Signed-off-by: Emory Freitas <emory.rvf@gmail.com>

--- api/src/main/java/com/sinch/gamehighscore/controller/ScoreController.java
+++ api/src/main/java/com/sinch/gamehighscore/controller/ScoreController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@CrossOrigin(origins = {"http//:localhost:8080", "http://localhost:3000"})
+@CrossOrigin(origins = {"http://localhost:8080", "http://localhost:3000"})
 @Controller
 @RequestMapping(path="/score")
 public class ScoreController {
