7675aa5e82eb7eac16b1d5af16fd09b70311541d
Emory Freitas <ervf@cesar.org.br>
2022-01-24 21:12:44 -0400

    Fix run docker
     -Fix api docker
    
    Signed-off-by: Emory Freitas <emory.rvf@gmail.com>

--- docker.sh
+++ docker.sh
@@ -6,6 +6,11 @@ cd api/
 docker build -t com.sinch.gamehighscore/gamehighscore-api .
 echo "Built api docker image..."
 
+echo "Building ui docker image..."
+cd ../ui/
+docker build -t com.sinch.gamehighscore/gamehighscore-ui .
+echo "Built api docker image..."
+
 
 cd ../
 echo "Running docker-compose up..."
